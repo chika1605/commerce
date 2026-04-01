08582a8a717729fbb173389e1f096cdfc93e9ad3
chika <152640307+chika1605@users.noreply.github.com>
2026-04-01 10:05:37 +0600

    order is added!

---
+++ common/src/test/java/kg/rubicon/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.rubicon.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
